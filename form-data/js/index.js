96fd0aba8ad17651f7a331a19cef9989aa22cd45
Jonatan Fragoso <jonatanfragoso@gmail.com>
2022-11-04 18:25:43 -0500

    retirando submit do botão.

--- form-data/js/index.js
+++ form-data/js/index.js
@@ -6,6 +6,9 @@ function enviar() {
     const y = formData.get("segundo")
     console.log(typeof y)
 
+    console.log("Primeiro número: ", parseFloat(x))
+    console.log("Segundo número: ", parseFloat(y))
+
     console.log("Soma: ", parseFloat(x) + parseFloat(y))
     console.log("Subtração: ", parseFloat(x) - parseFloat(y))
     console.log("Multiplicação: ", parseFloat(x) * parseFloat(y))
