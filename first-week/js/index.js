9be46288aae51414a02a6754eec2234d4304f527
Jonatan Fragoso <jonatanfragoso@gmail.com>
2022-11-04 18:10:10 -0500

    coomit do start da aula.

--- first-week/js/index.js
+++ first-week/js/index.js
@@ -1,110 +1,100 @@
 // Overview de JavaScript
 
-//console
+// console
 console.log('Texto dentro de um log.')
-console.error('Essa é uma mensagem de erro.')
 console.warn('Isso é um alerta.')
+console.error('Essa é uma mensagem de erro.')
+// console é um objeto e log(), warn() e error() são métodos ou funções
 
-//Variáveis
-const name = 'Jonatan'
-console.log(name)
+// variáveis
 
-
-
+// escopo global
 let x = 10
-
 if(true){
+    // escopo local
     let x = 20
+    console.log(x)
 }
-
-console.log(x)
-
+// é incorreto utilizar var depois do EC6
 
 // tipos de dados
 
-//string
-const n = 'Jonatan'
+// string
+const name = 'Victor'
+console.log(name)
+console.log(typeof name)
+
+// number
+let n = 10
+console.log(n)
+console.log(typeof n)
+n = 15.99
 console.log(n)
 console.log(typeof n)
 
-//number
-let num = 10
-console.log(num)
-console.log(typeof num)
-num = 15.99
-console.log(num)
-console.log(typeof num)
-
-//boolean
-const isOpen = true // 1 = true, 0 = false
+// bool
+const isOpen = 0
 console.log(isOpen)
 console.log(typeof isOpen)
 
-//null
+// null
 const a = null
 console.log(a)
 console.log(typeof a)
 
-//undefined
+// undefined 
 let nada
 console.log(nada)
 console.log(typeof nada)
+// deve-se evitar ter dados nessa configuração
 
-//array
-const linguagens = ['C++', 50, 'Java', 'Python', 'JS']
-console.log(linguagens[0])
-console.log(typeof linguagens[0])
+// array
+const linguagens = ['C++', 50, 'Python', 'JS']
 console.log(linguagens[1])
 console.log(typeof linguagens[1])
 
-//object literals
+// object literals
+// são usados quando a necessidade é de representar alguma entidade, como usuário, pessoa, produto
+// possuem notação específica, similar à do JSON
 const user = { 
-    username: 'borja',
-    password: 'senha',
-    age: 30,
-    name: 'Jonatan'
+    username:'vrc', 
+    password:'123', 
+    name:'Victor',
+    age:31
 }
-
 console.log(user)
 console.log(typeof user)
 
-//métodos de string
+// métodos de string
 const text = 'qualquer um'
+// length para retornar o tamanho de uma string; obs.: length é um atributo, e não um método
 console.log(text.length)
-//quebrar texto em um conjunto de caracteres
-const textToArray = text.split(' ')
-console.log(textToArray[0], textToArray[1])
-console.log(text.toLocaleUpperCase())
-
-//procurando algo dentro de uma string
+// split() para quebrar texto em um determinado conjunto de caracteres
+const textToArray = text.split('quer')
+console.log(textToArray[1])
+// toUpperCase() para transformar todos os caracteres em maiúsculo
+console.log(text.toUpperCase())
+// indexOf() para procurar algo dentro de uma string
 console.log(text.indexOf('abc'))
-console.log(text.indexOf('qualquer'))
+// charAt() para encontrar um char em uma posição específica dentro do vetor de caracteres que uma string é
 console.log(text.charAt(0))
-console.log(text.slice(0, 3))
+// slice() para encontrar um conjunto de caracteres num determinado intervalo do vetor da string
+console.log(text.slice(2,10))
 
-//mais sobre arays
-
-const chars = ['A', 'B', 'C']
+// mais sobre arrays
+// podem ser heterogêneos, ou seja, ter dados de tipos diferentes
+const chars = ['A', 'B', 'C', 'D', 88]
+// length em um array retorna quantos dados estão lá dentro
 console.log(chars.length)
+// para chegar na última posição, tamanho-1 como índice
 console.log(chars[chars.length-1])
+// mesmo que seja declarado como const, dá pra manipular um array
 chars[5] = 'F'
 console.log(chars)
+// pop para excluir do fim do array; shift para excluir do início do array
 chars.pop()
 chars.pop()
+chars.pop()
+// push para inserir no fim do array; unshift para inserir no início do array 
+chars.push('B')
 console.log(chars)
-chars.pop()
-chars.pop()
-chars.pop()
-console.log(chars)
-chars.unshift('B')
-console.log(chars)
-
-
-
-
-//pegando dados do form
-// const formData = new FormData(document.querySelector('form'))
-// const inputValue = formData.get("numero1")
-// const inputValue2 = formData.get("numero1")
-// console.log(inputValue)
-// console.log(inputValue2)
