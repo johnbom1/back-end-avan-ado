78a8132bf53c63b0866d5c256b518e45e40d68f3
Jonatan Fragoso <jonatanfragoso@gmail.com>
2022-11-14 20:41:53 -0500

    programção pareada 14/11 - project-favorites

--- project-favorites/server.js
+++ project-favorites/server.js
@@ -5,13 +5,24 @@ const fs = require('fs')
 
 http.createServer((req, res)=>{
 
+    const file = req.url === '/' ? 'index.html' : req.url
+    const filePath = path.join(__dirname, 'public', file)
+    const extname = path.extname(filePath)
+
+    const allowedFileTypes = ['.html', '.js', '.css']
+
+    const allowed = allowedFileTypes.find(
+        (item) => item === extname
+    )
+
+    if(!allowed) return
+
     //definindo rotas para a aplicação
-    if(req.url === '/')
-        fs.readFile(
-            path.join(__dirname, 'public', 'index.html'),
-            (err, content) => {
-                if (err) throw err
-                res.end(content)
-            }
-        )
+    fs.readFile(
+        filePath,
+        (err, content) => {
+            if (err) throw err
+            res.end(content)
+        }
+    )
 }).listen(5000,()=>(console.log('Servidor rodando...')))
