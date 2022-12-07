dd1c2b9b1b50e96983b4364b6936c77db5a2908d
Jonatan Fragoso <jonatanfragoso@gmail.com>
2022-12-06 19:45:17 -0500

    funcao para adicionar e deletar nome-url no JSON

--- project-favorites/api/index.js
+++ project-favorites/api/index.js
@@ -4,18 +4,31 @@ const fs = require('fs')
 const path = require('path')
 const data = require('./urls.json')
 
-
+ 
 
 http.createServer((req, res) => {
    
+    function writeFile(cb){
+        fs.writeFile(
+            path.join(__dirname, 'urls.json'),
+            JSON.stringify(data, null, 2),
+            err => {
+                if (err) throw err
+                res.end('Operação realizada com sucesso!')
+            }
+    
+        )
+    }
     const {name, url, del } = URL.parse(req.url, true).query
     
     if(!name || !url)
-        return res.end('show')
+        return res.end(JSON.stringify(data))
    
-    if(del)
-        return res.end('delete')
-   
-    return res.end('create')
+    if(del){
+        data.urls = data.urls.filter(item => item.url != url)
+        return writeFile(message => res.end(message))
+    }
+    data.urls.push({name, url})
+    return writeFile(message => res.end(message))
 
 }).listen(3000, () => console.log('API is running.'))
